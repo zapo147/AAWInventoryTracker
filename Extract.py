2ac1fac17d486ce6503539eda8bc239149d54d4e
Victor Tran <victor.tran91@gmail.com>
2013-06-13 22:09:01 -0400

    Added Extract class.

---
+++ Extract.py
@@ -0,0 +1,13 @@
+# Explain what Extract is #
+
+class Extract:
+    """A simple example class"""
+	def __init__(self, name, concentration):
+		self.name = name
+		self.concentration = concentration
+		
+	"""A simple test function"""
+	def test():
+		x = Extract("urine", 7.0)
+		print x.name
+		print x.concentration
